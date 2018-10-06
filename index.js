7592fec38e1c7aa4af3ecf5911eb7a06f2a0dc96
janerishbaluca <janerishbaluca@gmail.com>
2018-10-06 05:15:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
+<<<<<<< HEAD
   return `Happy holidays, ${name}!`
 }
 
@@ -13,4 +14,15 @@ function happyHolidayTo(holiday, name) {
 
 function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
+=======
+  console.log('Happy holidays, ${name}!')
+}
+
+function happyHolidayTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}!'
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days until ${holiday}!"
+>>>>>>> 9bcbdc4348ad4cb8239b473f9382fe3c974f40fb
 }
